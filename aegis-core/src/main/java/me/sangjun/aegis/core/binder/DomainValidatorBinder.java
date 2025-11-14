65094033e274a89d53b8d02f7556008d3969b73f
조상준 <juncho12011201@gmail.com>
2025-11-14 17:10:25 +0100

    feat(aegis-core): 도메인, Validator 1대1 대응 관계 검사 로직 구현

---
+++ aegis-core/src/main/java/me/sangjun/aegis/core/binder/DomainValidatorBinder.java
@@ -0,0 +1,61 @@
+package me.sangjun.aegis.core.binder;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import me.sangjun.aegis.core.api.DomainValidator;
+
+public class DomainValidatorBinder {
+
+    /**
+     * DomainValidator<T>의 구현체인 validators를 대상으로 T 타입(Class)을 리플렉션으로 추출한다. 그 이후, 도메인과 validator가 1대1로 정확히 대응하는지 검사한다.
+     */
+    public Map<Class<?>, Class<? extends DomainValidator>> bind(Set<Class<?>> domains,
+                                                                Set<Class<? extends DomainValidator>> validators) {
+
+        Map<Class<?>, Class<? extends DomainValidator>> domainValidatorMap = new HashMap<>();
+
+        for (Class<? extends DomainValidator> validator : validators) {
+            Class<?> domain = extractTypeArgumentFrom(validator);
+
+            if (domain == null) {
+                //TODO: 예외 로그 및 부트스트랩 실패
+            }
+
+            if (domainValidatorMap.containsKey(domain)) {
+                //TODO: 중복 Validator 예외 로그 및 부트스트랩 실패
+            }
+
+            if (!domains.contains(domain)) {
+                //TODO: 중복 Validator 예외 로그 및 부트스트랩 실패
+            }
+
+            domainValidatorMap.put(domain, validator);
+        }
+
+        return domainValidatorMap;
+    }
+
+    /**
+     * DomainValidator 구현체의 타입 변수를 추출하는 메소드. 즉, DomainValidator이 어느 도메인의 Validator인지 추출하는 메소드
+     *
+     * @param validator
+     * @return
+     */
+    private Class<?> extractTypeArgumentFrom(Class<? extends DomainValidator> validator) {
+        for (Type type : validator.getGenericInterfaces()) {
+            ParameterizedType pt = (ParameterizedType) type;
+            if (pt.getRawType().equals(DomainValidator.class)) {
+                Type argument = pt.getActualTypeArguments()[0];
+                if (argument instanceof Class<?> domainType) {
+                    return domainType;
+                }
+                //TODO: 예외 로그 및 부트스트랩 실패 및 하위 삭제
+            }
+        }
+
+        return null;
+    }
+}
--- aegis-core/src/main/java/me/sangjun/aegis/core/bootstrap/AegisBootStrap.java
+++ aegis-core/src/main/java/me/sangjun/aegis/core/bootstrap/AegisBootStrap.java
@@ -1,28 +1,53 @@
 package me.sangjun.aegis.core.bootstrap;
 
+import java.util.Map;
+import java.util.Set;
 import me.sangjun.aegis.core.api.AegisConfig;
+import me.sangjun.aegis.core.api.DomainValidator;
+import me.sangjun.aegis.core.binder.DomainValidatorBinder;
+import me.sangjun.aegis.core.scanner.DomainScanner;
+import me.sangjun.aegis.core.scanner.ValidatorScanner;
 
 public class AegisBootStrap {
 
+    public static void run(Class<?> primarySource) {
+        new AegisBootStrap().start(primarySource);
+    }
+
+    private final DomainScanner domainScanner;
+    private final ValidatorScanner validatorScanner;
+    private final DomainValidatorBinder domainValidatorBinder;
+
+    private AegisBootStrap() {
+        this.domainScanner = new DomainScanner();
+        this.validatorScanner = new ValidatorScanner();
+        this.domainValidatorBinder = new DomainValidatorBinder();
+    }
+
     /**
-     * Config 파일 없이 관리 대상 도메인을 등록하는 경우 사용 가능하다.
-     * 따라서, @AegisDomain을 기반으로만 관리 대상 도메인을 등록한다.
+     * Config 파일 없이 관리 대상 도메인을 등록하는 경우 사용 가능하다. 따라서, @AegisDomain을 기반으로만 관리 대상 도메인을 등록한다.
+     *
      * @param primarySource : 사용자 어플리케이션의 main클래스를 의미한다.
      */
-    public static void run(Class<?> primarySource) {
+    private void start(Class<?> primarySource) {
         /**
          * TODO:
          * 1. 관리 대상 도메인 등록
          * 2. 관리 대상에 대응하는 Validator 구현 여부 검사
          * 3. 관리 대상 도메인의 필드에 대응하는 validate 메소드 구현 여부 검사
          */
+        Set<Class<?>> domains = domainScanner.scanDomain(primarySource);
+        Set<Class<? extends DomainValidator>> validators = validatorScanner.scanValidator(primarySource);
+        Map<Class<?>, Class<? extends DomainValidator>> domainValidatorMapping = domainValidatorBinder.bind(domains,
+                validators);
     }
 
     /**
-     * 도메인 탐색을 Config 파일을 기반으로 관리 대상 도메인을 등록하는 경우 사용한다.
-     * Config 파일 외에도, primarySource를 기반으로 하위 @AegisDomain을 탐지하고 관리 대상 도메인을 등록한다.
+     * 도메인 탐색을 Config 파일을 기반으로 관리 대상 도메인을 등록하는 경우 사용한다. Config 파일 외에도, primarySource를 기반으로 하위 @AegisDomain을 탐지하고 관리 대상
+     * 도메인을 등록한다.
+     *
      * @param primarySource : 사용자 어플리케이션의 main클래스를 의미한다.
-     * @param config : AegisConfig의 구현체로 사용자가 선언 방식 혹은 path 탐색 방식으로 도메인을 등록하는 경우, 이에 대한 정보가 작성된 파일이다.
+     * @param config        : AegisConfig의 구현체로 사용자가 선언 방식 혹은 path 탐색 방식으로 도메인을 등록하는 경우, 이에 대한 정보가 작성된 파일이다.
      */
     public static void run(Class<?> primarySource, AegisConfig config) {
         /**
