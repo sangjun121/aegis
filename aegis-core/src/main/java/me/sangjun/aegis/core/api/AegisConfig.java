c470f8f041b7ec2fddd721a71c1a40e62f16fe38
조상준 <juncho12011201@gmail.com>
2025-11-15 18:08:19 +0100

    feat(aegis-core): 사용자에게 basePackage 경로 주입 config 작성

--- aegis-core/src/main/java/me/sangjun/aegis/core/api/AegisConfig.java
+++ aegis-core/src/main/java/me/sangjun/aegis/core/api/AegisConfig.java
@@ -3,12 +3,16 @@ package me.sangjun.aegis.core.api;
 import java.util.Collections;
 import java.util.List;
 
-public abstract class AegisConfig {
-    public List<String> baseDomainPaths() {
-        return Collections.emptyList();
-    }
+public interface AegisConfig {
+    List<String> basePackages();
 
-    public List<Class<?>> domains() {
-        return Collections.emptyList();
+    /**
+     * 어플리케이션 메인 클래스를 기준으로 basePackage를 결정하는 경우 별도 초기화 없이 해당 메소드 호출
+     * @param primarySource : 어플리케이션 메인 클래스
+     * @return
+     */
+    default List<String> defaultBasePackages(Class<?> primarySource) {
+        String rootPackage = primarySource.getPackageName();
+        return Collections.singletonList(rootPackage);
     }
 }
