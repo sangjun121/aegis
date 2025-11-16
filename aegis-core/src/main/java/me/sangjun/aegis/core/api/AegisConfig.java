9bb3f670417bd5cce7fac7ac13b6ca2c798db0c9
조상준 <juncho12011201@gmail.com>
2025-11-16 20:24:50 +0100

    refactor(aegis-core): 사용자 입력 basepackage 타입 중복값 제거를 위한 타입 변경

--- aegis-core/src/main/java/me/sangjun/aegis/core/api/AegisConfig.java
+++ aegis-core/src/main/java/me/sangjun/aegis/core/api/AegisConfig.java
@@ -1,22 +1,28 @@
 package me.sangjun.aegis.core.api;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public interface AegisConfig {
     /**
      * 입력되는 basePackage의 포멧은 FQCN을 준수해야 한다. 예: com.ryc.domain
+     *
      * @return
      */
-    List<String> basePackages();
+    Set<String> basePackages();
 
     /**
      * 어플리케이션 메인 클래스를 기준으로 basePackage를 결정하는 경우 별도 초기화 없이 해당 메소드 호출
+     *
      * @param primarySource : 어플리케이션 메인 클래스
      * @return
      */
-    default List<String> defaultBasePackages(Class<?> primarySource) {
+    default Set<String> defaultBasePackages(Class<?> primarySource) {
+        Set<String> defaults = new HashSet<>();
+
         String rootPackage = primarySource.getPackageName();
-        return Collections.singletonList(rootPackage);
+        defaults.add(rootPackage);
+
+        return defaults;
     }
 }
