617a5c7c5ab7f4e252159566a3a1b3d6ba44ee6b
조상준 <juncho12011201@gmail.com>
2025-11-16 20:37:16 +0100

    refactor(aegis-core): Scanner 구현 방식 ClassGraph로 전체적인 수정

--- aegis-core/src/main/java/me/sangjun/aegis/core/scanner/DomainScanner.java
+++ aegis-core/src/main/java/me/sangjun/aegis/core/scanner/DomainScanner.java
@@ -1,5 +1,7 @@
 package me.sangjun.aegis.core.scanner;
 
+import static me.sangjun.aegis.core.exception.AegisErrorMessage.CLASS_NOT_FOUND;
+
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ClassInfoList;
@@ -7,6 +9,7 @@ import io.github.classgraph.ScanResult;
 import java.util.HashSet;
 import java.util.Set;
 import me.sangjun.aegis.core.annotations.AegisDomain;
+import me.sangjun.aegis.core.exception.AegisException;
 
 public class DomainScanner {
 
@@ -16,7 +19,7 @@ public class DomainScanner {
      * @param primarySource : 사용자 어플리케이션의 main 클래스이다.
      * @return 스캔된 도메인을 제공한다.
      */
-    public Set<Class<?>> scanDomain(Class<?> primarySource, Set<String> basePackages) {
+    public Set<Class<?>> scan(Class<?> primarySource, Set<String> basePackages) {
         Set<Class<?>> domains = new HashSet<>();
 
         ClassLoader loader = primarySource.getClassLoader(); // 해당 ClassLoader는 단지 도메인을 저장하기 위해서만 사용.
@@ -27,18 +30,17 @@ public class DomainScanner {
                 .acceptPackages(basePackages.toArray(new String[0])); // 스캔 범위를 basePackages 하위로 선정
 
         try (ScanResult scanResult = classGraph.scan()) {
-
             ClassInfoList domainClassInfos =
                     scanResult.getClassesWithAnnotation(AegisDomain.class.getName()); // @AegisDomain 붙은 클래스 가져오기
 
             for (ClassInfo classInfo : domainClassInfos) {
                 String className = classInfo.getName();
 
-                Class<?> clazz = Class.forName(className, false, loader);
+                Class<?> clazz = Class.forName(className, false, loader); // 초기화를 제외한 로딩
                 domains.add(clazz);
             }
         } catch (ClassNotFoundException e) {
-            //TODO: 예외 및 로그 처리
+            throw new AegisException(CLASS_NOT_FOUND.getMessage());
         }
 
         return domains;
--- aegis-core/src/main/java/me/sangjun/aegis/core/scanner/ValidatorScanner.java
+++ aegis-core/src/main/java/me/sangjun/aegis/core/scanner/ValidatorScanner.java
@@ -1,40 +1,52 @@
 package me.sangjun.aegis.core.scanner;
 
-import static me.sangjun.aegis.core.exception.AegisErrorMessage.INVALID_CLASS_NAME;
+import static me.sangjun.aegis.core.exception.AegisErrorMessage.CLASS_NOT_FOUND;
 
-import java.nio.file.Path;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import me.sangjun.aegis.core.api.DomainValidator;
 import me.sangjun.aegis.core.exception.AegisException;
-import me.sangjun.aegis.core.util.ClassScanner;
 
 public class ValidatorScanner {
 
-    public Set<Class<?>> scanValidator(Class<?> primarySource) {
+    /**
+     * scanValidator는 라이브러리를 제외한 순수 구현으로 작성.
+     */
+    public Set<Class<?>> scan(Class<?> primarySource, Set<String> basePackages) {
         Set<Class<?>> validators = new HashSet<>();
 
-        Path rootPath = ClassScanner.resolveClassesRoot(primarySource);
-        List<Path> classFilePaths = ClassScanner.getClassFilePaths(rootPath);
+        ClassLoader loader = primarySource.getClassLoader();
 
-        for (Path path : classFilePaths) {
-            String className = ClassScanner.parseClassName(rootPath, path);
-            try {
-                Class<?> actualClass = Class.forName(className);
-                if (isDomainValidator(actualClass)) {
-                    validators.add(actualClass);
-                }
-            } catch (ClassNotFoundException e) {
-                throw new AegisException(INVALID_CLASS_NAME.getMessage());
+        ClassGraph classGraph = new ClassGraph()
+                .enableClassInfo()
+                .acceptPackages(basePackages.toArray(new String[0]));
+
+        try (ScanResult scanResult = classGraph.scan()) {
+            ClassInfoList validatorClassInfos =
+                    scanResult.getClassesImplementing(DomainValidator.class.getName());
+
+            for (ClassInfo classInfo : validatorClassInfos) {
+                String className = classInfo.getName();
+
+                Class<?> clazz = Class.forName(className, false, loader);
+                validators.add(clazz);
             }
+        } catch (ClassNotFoundException e) {
+            throw new AegisException(CLASS_NOT_FOUND.getMessage());
         }
 
         return validators;
     }
 
+    /**
+     * @deprecated 내부 스캐닝 로직에 라이브러리를 도입하여 더 이상 사용되지 않습니다. {@link ValidatorScanner} scan에 구현된 ClassGraph 기반 스캐너를 사용.
+     */
+    @Deprecated
     private boolean isDomainValidator(Class<?> cls) {
-        // 인터페이스 자체는 검색 대상에서 제거
         if (cls.isInterface()) {
             return false;
         }
