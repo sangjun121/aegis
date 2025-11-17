e897779c7a35330fd9ef378a2ff0dd0577524691
조상준 <juncho12011201@gmail.com>
2025-11-17 22:28:00 +0100

    test(aegis-core): 의존 도메인 스캔 정상 테스트 실행

--- aegis-core/src/test/java/me/sangjun/aegis/core/api/AegisConfigTest.java
+++ aegis-core/src/test/java/me/sangjun/aegis/core/api/AegisConfigTest.java
@@ -3,20 +3,21 @@ package me.sangjun.aegis.core.api;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Set;
+import me.sangjun.aegis.core.SamplePrimary;
 import org.junit.jupiter.api.Test;
 
-class AegisConfigTest {
+public class AegisConfigTest {
 
-    class DefaultAegisConfig implements AegisConfig {
+    public static class DefaultAegisConfig implements AegisConfig {
         @Override
         public Set<String> basePackages() {
-            return defaultBasePackages(AegisConfigTest.class);
+            return defaultBasePackages(SamplePrimary.class);
         }
     }
 
     @Test
     void 메인클래스를_기반으로_basePackage가_올바르게_설정된다(){
-        Set<String> thisClassPath = Set.of("me.sangjun.aegis.core.api");
+        Set<String> thisClassPath = Set.of("me.sangjun.aegis.core");
 
         AegisConfig defaultAegisConfig = new DefaultAegisConfig();
 
