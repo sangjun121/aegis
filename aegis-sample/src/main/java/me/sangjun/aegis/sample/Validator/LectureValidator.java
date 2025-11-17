f406ca2eba4528631af0f8588406591637199f8f
조상준 <juncho12011201@gmail.com>
2025-11-18 00:36:40 +0100

    refactor(aegis-sample): 샘플 프로젝트에 의존 관계 추가

---
+++ aegis-sample/src/main/java/me/sangjun/aegis/sample/Validator/LectureValidator.java
@@ -0,0 +1,36 @@
+package me.sangjun.aegis.sample.Validator;
+
+import java.util.List;
+import me.sangjun.aegis.core.api.DomainValidator;
+import me.sangjun.aegis.sample.domain.Lecture;
+
+public class LectureValidator implements DomainValidator<Lecture> {
+    @Override
+    public void validate(Lecture target) {
+        validateId(target.getId());
+        validateName(target.getName());
+        validateProfessorName(target.getProfessorName());
+        validateBlackListStudents(target.getBlackListStudents());
+    }
+
+    private void validateId(Long id) {
+        // 내부 검증 로직 구현
+    }
+
+    private void validateName(String name) {
+        // 내부 검증 로직 구현
+    }
+
+    private void validateProfessorName(String professorName) {
+        // 내부 검증 로직 구현
+    }
+
+    private void validateStudents(List<String> students) {
+        // 내부 검증 로직 구현
+    }
+
+    private void validateBlackListStudents(List<String> blackListStudents) {
+        // 내부 검증 로직 구현
+    }
+}
+
